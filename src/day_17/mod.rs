61384c4cbd576a96937c42c7940ec3dbee80f39a
Sebastian Schmidt <isibboi@gmail.com>
2021-12-21 12:35:51 +0100

    Add templates for all remaining days.

---
+++ src/day_17/mod.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn problem_1() {
+    let lines = BufReader::new(File::open("inputs/17.txt").unwrap()).lines();
+
+    println!("Day 17 problem 1: {}", 0);
+}
+
+pub fn problem_2() {
+    let lines = BufReader::new(File::open("inputs/17.txt").unwrap()).lines();
+
+    println!("Day 17 problem 2: {}", 0);
+}
